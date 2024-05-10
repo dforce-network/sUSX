09560e6c0b3ee4177c93f3e485ce1ba0cfc1da2f
Skyge <1506186404li@gmail.com>
2024-05-10 20:56:42 +0800

    :art: Add the limitation on the validity period of interest rates.

--- src/USXSavingRate.sol
+++ src/USXSavingRate.sol
@@ -6,13 +6,15 @@ import {Ownable2StepUpgradeable} from "@openzeppelin/contracts-upgradeable/acces
 
 contract USXSavingRate is Initializable, Ownable2StepUpgradeable {
     uint256 private constant RAY = 10 ** 27;
-    int256 private constant MAX_USR = 10 ** 27;
-    int256 private constant MIN_USR = -1 * 10 ** 27;
+    uint256 private constant MAX_USR = 2 * 10 ** 27;
+    uint256 private constant MIN_USR = 0;
 
-    int256 public usr;  // USX Savings Rate at the current period
+    uint256 public usr;  // USX Savings Rate at the current period
+    uint256 public usrExpiredTime;  // The time of the USX Savings Rate expired
     uint256 public rateAccumulator;  // Accumulated USX Savings Rate
-    int256 public nextUsr;  // USX Savings Rate at the next period 
-    uint256 public nextUsrTime;  // The time of the USX Savings Rate works on at the next period
+    uint256 public nextUsr;  // USX Savings Rate at the next period 
+    uint256 public nextUsrWorkingTime;  // The time of the USX Savings Rate works at the next period
+    uint256 public nextUsrExpiredTime;  // The time of the USX Savings Rate expired at the next period
     uint256 public lastAccumulatedTime;  // time of last drip
 
     mapping(address => uint256) public userSavingsUsx;  // User Savings USX
@@ -21,20 +23,32 @@ contract USXSavingRate is Initializable, Ownable2StepUpgradeable {
         _disableInitializers();
     }
 
-    function initialize() external initializer {
+    function initialize(
+        uint256 _usr,
+        uint256 _usrExpiredTime
+    ) external initializer {
+        require(_usr > MIN_USR && _usr < MAX_USR, "Invalid usr value!");
+        require(_usrExpiredTime > block.timestamp, "Invalid usr expired time!");
         __Ownable2Step_init();
 
+        usr = _usr;
+        usrExpiredTime = _usrExpiredTime;
         rateAccumulator = RAY;
         lastAccumulatedTime = block.timestamp;
     }
 
-    function _setNextUsr(int256 _nextUsr, uint256 _nextUsrTime) external onlyOwner {
-        require(_nextUsrTime > block.timestamp, "Invalid next usr time!");
+    function _setNextUsr(uint256 _nextUsr, uint256 _nextUsrWorkingTime, uint256 _nextUsrExpiredTime) external onlyOwner {
+        require(_nextUsrWorkingTime > block.timestamp, "Invalid next usr working time!");
+        require(_nextUsrExpiredTime > block.timestamp, "Invalid next usr expired time!");
         require(_nextUsr > MIN_USR && _nextUsr < MAX_USR, "Invalid next usr value!");
         accumulateUsr();
 
         nextUsr = _nextUsr;
-        nextUsrTime = _nextUsrTime;
+        nextUsrWorkingTime = _nextUsrWorkingTime;
+        nextUsrExpiredTime = _nextUsrExpiredTime;
+        if (usrExpiredTime > _nextUsrWorkingTime) {
+            usrExpiredTime = _nextUsrWorkingTime;
+        }
     }
 
     function _rpow(uint256 x, uint256 n, uint256 base) internal pure returns (uint256 z) {
@@ -69,25 +83,28 @@ contract USXSavingRate is Initializable, Ownable2StepUpgradeable {
         require(y == 0 || (z = x * y) / y == x);
     }
 
-    function accumulateUsr() public returns (uint256 _newUsr) {
+    function accumulateUsr() public returns (uint256 _newRateAccumulator) {
         require(lastAccumulatedTime <= block.timestamp, "Invalid last accumulated time!");
-
         if (lastAccumulatedTime == block.timestamp) {
-            _newUsr = rateAccumulator;
-            return _newUsr;
+            _newRateAccumulator = rateAccumulator;
         } else {
-            // Always safe to cast to uint256 because usr is always between MIN_USR and MAX_USR
-            uint256 finalUSR = uint256(int256(RAY) + usr);
-            if (block.timestamp < nextUsrTime || nextUsrTime == 0) {
-                _newUsr = _rmul(_rpow(finalUSR, block.timestamp - lastAccumulatedTime, RAY), rateAccumulator);
+            uint256 accumulatedEndTime = block.timestamp > usrExpiredTime ? usrExpiredTime : block.timestamp;
+            uint256 elapsedTime = accumulatedEndTime - lastAccumulatedTime;
+            if (block.timestamp < nextUsrWorkingTime || nextUsrWorkingTime == 0) {
+                _newRateAccumulator = _rmul(_rpow(usr, elapsedTime, RAY), rateAccumulator);
             } else {
-                _newUsr = _rmul(_rpow(finalUSR, nextUsrTime - lastAccumulatedTime, RAY), rateAccumulator);
-                finalUSR = uint256(int256(RAY) + nextUsr);
-                _newUsr = _rmul(_rpow(finalUSR, block.timestamp - nextUsrTime, RAY), _newUsr);
+                _newRateAccumulator = _rmul(_rpow(usr, elapsedTime, RAY), rateAccumulator);
+                accumulatedEndTime = block.timestamp > nextUsrExpiredTime ? nextUsrExpiredTime : block.timestamp;
+                elapsedTime = accumulatedEndTime - nextUsrWorkingTime;
+                _newRateAccumulator = _rmul(_rpow(nextUsr, elapsedTime, RAY), _newRateAccumulator);
+
+                usr = nextUsr;
+                usrExpiredTime = nextUsrExpiredTime;
+                nextUsrWorkingTime = 0;
             }
 
-            rateAccumulator = _newUsr;
-            lastAccumulatedTime = block.timestamp;
+            rateAccumulator = _newRateAccumulator;
+            lastAccumulatedTime = block.timestamp > usrExpiredTime ? usrExpiredTime : block.timestamp;
         }
     }
 }
