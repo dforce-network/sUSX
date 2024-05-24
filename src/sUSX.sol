6f149af6c854b8d500d951ffe40c0de929422797
Skyge <1506186404li@gmail.com>
2024-05-24 16:30:27 +0800

    :building_construction: Update contract to simplify rate calculation.

--- src/sUSX.sol
+++ src/sUSX.sol
@@ -10,11 +10,11 @@ import {PausableUpgradeable} from "@openzeppelin/contracts-upgradeable/security/
 import {SafeERC20Upgradeable} from "@openzeppelin/contracts-upgradeable/token/ERC20/utils/SafeERC20Upgradeable.sol";
 import {IERC1271} from "./interface/IERC1271.sol";
 import {IMSD,IMSDController} from "./interface/IMSDMintable.sol";
+import "./helper/MathHelper.sol";
 
-contract sUSX is Initializable, Ownable2StepUpgradeable, PausableUpgradeable, ERC20PermitUpgradeable, ERC4626Upgradeable {
+contract sUSX is Initializable, Ownable2StepUpgradeable, PausableUpgradeable, ERC20PermitUpgradeable, ERC4626Upgradeable, MathHelper {
     using MathUpgradeable for uint256;
 
-    uint256 private constant RAY = 10 ** 27;
     uint256 private constant MAX_USR = 2 * 10 ** 27;
     uint256 private constant MIN_USR = 0;
 
@@ -23,26 +23,27 @@ contract sUSX is Initializable, Ownable2StepUpgradeable, PausableUpgradeable, ER
     uint256 internal totalUnstaked;
     uint256 public mintCap; // Cap to mint sUSX
 
-    uint256 public accumulatedRate;
-
     struct UsrDetail {
         uint256 startTime;
         uint256 endTime;
         uint256 usr;
+        uint256 startRate;
     }
 
     UsrDetail[] public usrDetails;
+    uint256 public lastEpochId;
 
-    modifier updateRate() {
-        uint256 length = usrDetails.length;
-
-        if (block.timestamp > usrDetails[length - 1].endTime) {
-            accumulatedRate = getAccumulatedRateByTime(block.timestamp);
+    modifier updateRate(uint256 _time) {
+        (uint256 currentEpochId, ) = getRateByEpochId(lastEpochId, _time);
+        if (currentEpochId > lastEpochId) {
+            lastEpochId = currentEpochId;
         }
         _;
     }
 
     constructor(
+        string memory _name,
+        string memory _symbol,
         IERC20Upgradeable _usx,
         address _msdController,
         uint256 _mintCap,
@@ -50,10 +51,12 @@ contract sUSX is Initializable, Ownable2StepUpgradeable, PausableUpgradeable, ER
         uint256 _initialUsrEndTime,
         uint256 _initialUsr
     ) {
-        initialize(_usx, _msdController, _mintCap, _initialUsrStartTime, _initialUsrEndTime, _initialUsr);
+        initialize(_name, _symbol, _usx, _msdController, _mintCap, _initialUsrStartTime, _initialUsrEndTime, _initialUsr);
     }
 
     function initialize(
+        string memory _name,
+        string memory _symbol,
         IERC20Upgradeable _usx,
         address _msdController,
         uint256 _mintCap,
@@ -62,22 +65,23 @@ contract sUSX is Initializable, Ownable2StepUpgradeable, PausableUpgradeable, ER
         uint256 _initialUsr
     ) public initializer {
         require(_initialUsrStartTime >= block.timestamp, "Invalid usr start time!");
-        require(_initialUsrEndTime > block.timestamp, "Invalid usr end time!");
+        require(_initialUsrEndTime > _initialUsrStartTime, "Invalid usr end time!");
         require(_initialUsr > MIN_USR && _initialUsr < MAX_USR, "Invalid usr value!");
         
         __Ownable2Step_init();
         __Pausable_init();
-        __ERC20Permit_init("sUSX");
+        __ERC20Permit_init(_symbol);
         __ERC4626_init(_usx);
-        __ERC20_init("USX Savings", "sUSX");
+        __ERC20_init(_name, _symbol);
 
-        accumulatedRate = RAY;
         msdController = _msdController;
         mintCap = _mintCap;
+        lastEpochId = 0;
         usrDetails.push(UsrDetail({
             startTime: _initialUsrStartTime,
             endTime: _initialUsrEndTime,
-            usr: _initialUsr
+            usr: _initialUsr,
+            startRate: RAY
         }));
     }
 
@@ -101,102 +105,67 @@ contract sUSX is Initializable, Ownable2StepUpgradeable, PausableUpgradeable, ER
         uint256 _newUsrStartTime,
         uint256 _newUsrEndTime,
         uint256 _newUsr
-    ) external onlyOwner updateRate {
+    ) external onlyOwner {
         require(_newUsrStartTime >= block.timestamp, "Invalid new usr start time!");
-        require(_newUsrEndTime > block.timestamp, "Invalid new usr end time!");
+        require(_newUsrEndTime > _newUsrStartTime, "Invalid new usr end time!");
         require(_newUsr > MIN_USR && _newUsr < MAX_USR, "Invalid new usr value!");
 
         uint256 _length = usrDetails.length;
         // Length always is greater than 1.
-        if (usrDetails[_length - 1].endTime > _newUsrStartTime) {
-            usrDetails[_length - 1].endTime = _newUsrStartTime;
+        uint256 _lastEpochEndTime = usrDetails[_length - 1].endTime;
+        if (_lastEpochEndTime > _newUsrStartTime) {
+            _lastEpochEndTime = _newUsrStartTime;
+        }
+
+        (uint256 _currentEpochId, uint256 _newRate) = getRateByEpochId(lastEpochId, _lastEpochEndTime);
+        if (_currentEpochId > lastEpochId) {
+            lastEpochId = _currentEpochId;
         }
 
         usrDetails.push(UsrDetail({
             startTime: _newUsrStartTime,
             endTime: _newUsrEndTime,
-            usr: _newUsr
+            usr: _newUsr,
+            startRate: _newRate
         }));
     }
 
-    function _rpow(uint256 x, uint256 n, uint256 base) internal pure returns (uint256 z) {
-        assembly {
-            switch x case 0 {switch n case 0 {z := base} default {z := 0}}
-            default {
-                switch mod(n, 2) case 0 { z := base } default { z := x }
-                let half := div(base, 2)  // for rounding.
-                for { n := div(n, 2) } n { n := div(n,2) } {
-                    let xx := mul(x, x)
-                    if iszero(eq(div(xx, x), x)) { revert(0,0) }
-                    let xxRound := add(xx, half)
-                    if lt(xxRound, xx) { revert(0,0) }
-                    x := div(xxRound, base)
-                    if mod(n,2) {
-                        let zx := mul(z, x)
-                        if and(iszero(iszero(x)), iszero(eq(div(zx, x), z))) { revert(0,0) }
-                        let zxRound := add(zx, half)
-                        if lt(zxRound, zx) { revert(0,0) }
-                        z := div(zxRound, base)
-                    }
-                }
+    function getRateByEpochId(uint256 _startEpochId, uint256 _timestamp) public view returns (uint256 _epochId, uint256 _rateAccumulator) {
+        UsrDetail memory _usrInfo;
+        uint256 _elapsedTime;
+
+        uint256 _length = usrDetails.length;
+        for (_epochId =_startEpochId; _epochId < _length; ) {
+            _usrInfo = usrDetails[_epochId];
+
+            if (_timestamp <= _usrInfo.endTime) {
+                _elapsedTime = _timestamp < _usrInfo.startTime ? 0 : _timestamp - _usrInfo.startTime;
+                break;
+            } else {
+                _elapsedTime = _usrInfo.endTime - _usrInfo.startTime;
+            }
+
+            unchecked {
+                ++_epochId;
             }
         }
-    }
-
-    function _rmul(uint256 x, uint256 y) internal pure returns (uint256 z) {
-        z = _mul(x, y) / RAY;
-    }
-
-    function _mul(uint256 x, uint256 y) internal pure returns (uint256 z) {
-        require(y == 0 || (z = x * y) / y == x);
-    }
-
-    function getAccumulatedRateByTime(uint256 _timestamp) public view returns (uint256 _rateAccumulator) {
-        uint256 length = usrDetails.length;
-
-        if (length == 1) {
-            UsrDetail memory initialUsr = usrDetails[0];
-            uint256 accumulatedEndTime = _timestamp > initialUsr.endTime ? initialUsr.endTime : _timestamp;
-            uint256 elapsedTime = accumulatedEndTime > initialUsr.startTime ? accumulatedEndTime - initialUsr.startTime : 0;
-
-            _rateAccumulator = _rmul(_rpow(initialUsr.usr, elapsedTime, RAY), accumulatedRate);
-        } else {
-            UsrDetail memory newestUsr = usrDetails[length - 1];
-            UsrDetail memory newerUsr = usrDetails[length - 2];
-
-            require(_timestamp >= newerUsr.startTime, "Only the last 2 epochs can be accessed!");
-
-            uint256 accumulatedEndTime = _timestamp > newerUsr.endTime ? newerUsr.endTime : _timestamp;
-            uint256 elapsedTime = accumulatedEndTime - newerUsr.startTime;
-            _rateAccumulator = _rmul(_rpow(newerUsr.usr, elapsedTime, RAY), accumulatedRate);
-
-            if (_timestamp > newestUsr.startTime) {
-                accumulatedEndTime = _timestamp > newestUsr.endTime ? newestUsr.endTime : _timestamp;
-                elapsedTime = accumulatedEndTime - newestUsr.startTime;
-                _rateAccumulator = _rmul(_rpow(newestUsr.usr, elapsedTime, RAY), _rateAccumulator);
-            }
-        }
+        _rateAccumulator = _rmul(_rpow(_usrInfo.usr, _elapsedTime, RAY), _usrInfo.startRate);
     }
 
     function currentAccumulatedRate() external view returns (uint256 _rateAccumulator) {
-        _rateAccumulator = getAccumulatedRateByTime(block.timestamp);
+        (, _rateAccumulator) = getRateByEpochId(lastEpochId, block.timestamp);
     }
 
     function currentInterestRate() public view returns (uint256 _interestRate, uint256 _startTime, uint256 _endTime) {
-        uint256 length = usrDetails.length;
-        if (block.timestamp > usrDetails[length - 1].startTime &&
-            block.timestamp < usrDetails[length - 1].endTime
+        (uint256 currentEpochId, ) = getRateByEpochId(lastEpochId, block.timestamp);
+        UsrDetail memory usrInfo = usrDetails[currentEpochId];
+
+        if (block.timestamp >= usrInfo.startTime &&
+            block.timestamp < usrInfo.endTime
         ) {
-            _interestRate = usrDetails[length - 1].usr;
-            _startTime = usrDetails[length - 1].startTime;
-            _endTime = usrDetails[length - 1].endTime;
-        } else if (length > 1 &&
-            block.timestamp > usrDetails[length - 2].startTime &&
-            block.timestamp < usrDetails[length - 2].endTime
-        ) {
-            _interestRate = usrDetails[length - 2].usr;
-            _startTime = usrDetails[length - 2].startTime;
-            _endTime = usrDetails[length - 2].endTime;
+            _interestRate = usrInfo.usr;
+            _startTime = usrInfo.startTime;
+            _endTime = usrInfo.endTime;
         }
     }
 
@@ -242,12 +211,12 @@ contract sUSX is Initializable, Ownable2StepUpgradeable, PausableUpgradeable, ER
     }
 
     function _convertToAssets(uint256 shares, MathUpgradeable.Rounding rounding) internal view override returns (uint256) {
-        uint256 rateAccumulator = getAccumulatedRateByTime(block.timestamp);
+        (, uint256 rateAccumulator) = getRateByEpochId(lastEpochId, block.timestamp);
         return shares.mulDiv(rateAccumulator, RAY, rounding);
     }
 
     function _convertToShares(uint256 assets, MathUpgradeable.Rounding rounding) internal view override returns (uint256) {
-        uint256 rateAccumulator = getAccumulatedRateByTime(block.timestamp);
+        (, uint256 rateAccumulator) = getRateByEpochId(lastEpochId, block.timestamp);
         return assets.mulDiv(RAY, rateAccumulator, rounding);
     }
 
@@ -259,21 +228,21 @@ contract sUSX is Initializable, Ownable2StepUpgradeable, PausableUpgradeable, ER
         return mintCap - totalSupply();
     }
 
-    function deposit(uint256 assets, address receiver) public whenNotPaused updateRate override returns (uint256 shares) {
+    function deposit(uint256 assets, address receiver) public whenNotPaused updateRate(block.timestamp) override returns (uint256 shares) {
         shares = _convertToShares(assets, MathUpgradeable.Rounding.Down);
 
         require(shares + totalSupply() <= mintCap, "Exceeds mint cap!");
         _deposit(_msgSender(), receiver, assets, shares);
     }
 
-    function mint(uint256 shares, address receiver) public whenNotPaused updateRate override returns (uint256 assets){
+    function mint(uint256 shares, address receiver) public whenNotPaused updateRate(block.timestamp) override returns (uint256 assets){
         require(shares <= maxMint(receiver), "Exceeds mint cap!");
 
         assets = _convertToAssets(shares, MathUpgradeable.Rounding.Up);
         _deposit(_msgSender(), receiver, assets, shares);
     }
 
-    function withdraw(uint256 assets, address receiver, address owner) public whenNotPaused updateRate override returns (uint256 shares) {
+    function withdraw(uint256 assets, address receiver, address owner) public whenNotPaused updateRate(block.timestamp) override returns (uint256 shares) {
         require(assets <= maxWithdraw(owner), "Withdraw more than max");
 
         shares = _convertToShares(assets, MathUpgradeable.Rounding.Up);
@@ -281,7 +250,7 @@ contract sUSX is Initializable, Ownable2StepUpgradeable, PausableUpgradeable, ER
         _withdraw(_msgSender(), receiver, owner, assets, shares);
     }
 
-    function redeem(uint256 shares, address receiver, address owner) public whenNotPaused updateRate override returns (uint256 assets) {
+    function redeem(uint256 shares, address receiver, address owner) public whenNotPaused updateRate(block.timestamp) override returns (uint256 assets) {
         require(shares <= maxRedeem(owner), "Redeem more than max");
 
         assets = _convertToAssets(shares, MathUpgradeable.Rounding.Down);
@@ -294,24 +263,22 @@ contract sUSX is Initializable, Ownable2StepUpgradeable, PausableUpgradeable, ER
     }
 
     function currentAPY() external view returns (uint256 apy, uint256 startTime, uint256 endTime) {
-        uint256 secondsPerYear = 365 * 24 * 60 * 60;
-        uint256 interestRate;
-        (interestRate, startTime, endTime) = currentInterestRate();
+        uint256 _secondsPerYear = 365 * 24 * 60 * 60;
+        uint256 _interestRate;
+        (_interestRate, startTime, endTime) = currentInterestRate();
 
-        apy = _rpow(interestRate, secondsPerYear, RAY);
+        apy = _rpow(_interestRate, _secondsPerYear, RAY);
     }
 
     function nextAPY() external view returns (uint256 apy, uint256 startTime, uint256 endTime) {
-        uint256 secondsPerYear = 365 * 24 * 60 * 60;
-        uint256 length = usrDetails.length;
+        uint256 _secondsPerYear = 365 * 24 * 60 * 60;
+        uint256 _length = usrDetails.length;
 
-        if (length > 1) {
-            UsrDetail memory newestUsr = usrDetails[length - 1];
-            if (block.timestamp < newestUsr.startTime) {
-                apy = _rpow(newestUsr.usr, secondsPerYear, RAY);
-                startTime = newestUsr.startTime;
-                endTime = newestUsr.endTime;
-            }
+        UsrDetail memory _newestUsr = usrDetails[_length - 1];
+        if (block.timestamp < _newestUsr.startTime) {
+            apy = _rpow(_newestUsr.usr, _secondsPerYear, RAY);
+            startTime = _newestUsr.startTime;
+            endTime = _newestUsr.endTime;
         }
     }
 }
