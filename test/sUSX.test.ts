5a66626435e33014932361991c6713eea5690c96
Skyge <1506186404li@gmail.com>
2024-05-28 20:42:02 +0800

    :white_check_mark: Add test case for withdraw.

--- test/sUSX.test.ts
+++ test/sUSX.test.ts
@@ -22,17 +22,25 @@ const setup = deployments.createFixture(async () => {
 
 describe('sUSX', function () {
     let owner: any;
-    let user1:any;
+    let user1: any;
+    let alice: any;
+    let bob: any;
     let RAY = ethers.BigNumber.from("1000000000000000000000000000"); // 1e27
 
     before(async function () {
       const {deployer, sUSX, users, usx} = await setup();
       owner = deployer;
       user1 = users[1];
+      alice = users[10];
+      bob = users[11];
 
       expect(await owner.sUSX.usrConfigsLength()).to.eq("1");
       // Approve to sUSX to deposit
       await user1.usx.approve(user1.sUSX.address, ethers.constants.MaxUint256);
+      // Approve to sUSX to deposit
+      await alice.usx.approve(user1.sUSX.address, ethers.constants.MaxUint256);
+      // Approve to sUSX to deposit
+      await bob.usx.approve(user1.sUSX.address, ethers.constants.MaxUint256);
     });
 
     describe("Deposit", async function () {
@@ -49,17 +57,17 @@ describe('sUSX', function () {
 
         // Just deploy the whole contracts, 
         // so increase timestamp manually to increase usr accumulated rate.
-        let beforeAccumulatedRate = await user1.sUSX.currentAccumulatedRate();
-        let usrDetails = await user1.sUSX.usrDetails(0);
+        let beforeAccumulatedRate = await user1.sUSX.currentRate();
+        let usrDetails = await user1.sUSX.usrConfigs(0);
         let toIncreaseTime = usrDetails.startTime - await getCurrentTime();
 
         // Increase time to accumulate rate.
         await increaseTime(toIncreaseTime + 1);
         await increaseBlock(1);
-        expect(await user1.sUSX.currentAccumulatedRate()).to.gt(beforeAccumulatedRate);
+        expect(await user1.sUSX.currentRate()).to.gt(beforeAccumulatedRate);
 
         // Deposit: check sUSX
-        let nextRate = await user1.sUSX.getAccumulatedRateByTime((await getCurrentTime())+1);
+        let nextRate = await user1.sUSX.getRateByTime((await getCurrentTime())+1);
         let shareMintedAmount = depositAmount.mul(RAY).div(nextRate);
         await expect( 
           user1.sUSX.deposit(depositAmount, user1.address)
@@ -78,7 +86,7 @@ describe('sUSX', function () {
         ).to.changeTokenBalance(user1.sUSX, user1.address, 0);
       });
 
-      it("Deposit revert when pause contract", async function() {
+      it("Deposit revert when contract is paused", async function() {
         await owner.sUSX.pause();
         expect(await owner.sUSX.paused()).to.be.true;
 
@@ -111,29 +119,34 @@ describe('sUSX', function () {
         await user1.usx.mint(user1.address, faucetAmount.add("1"));
 
         // When call `deposit()`, the rate will accumulate for 1 second more
-        let nextRate = await user1.sUSX.getAccumulatedRateByTime((await getCurrentTime()) + 1);
+        let nextRate = await user1.sUSX.getRateByTime((await getCurrentTime())+1);
         let mintCap = await user1.sUSX.mintCap();
         let totalSupply = await user1.sUSX.totalSupply();
         let maxDepositAmount = mintCap.sub(totalSupply).mul(nextRate).div(RAY);
         // It will be rounded down when calculate the share amount,
-        // so deposit 1 more to cost the whole mint cap.
-        await user1.sUSX.deposit(maxDepositAmount.add("1"), user1.address);
-
-        maxDepositAmount = await user1.sUSX.maxDeposit(user1.address);
-        expect(maxDepositAmount).to.eq("0");
+        // so deposit `maxDepositAmount` will leave 1 wei share to reach the whole mint cap.
+        await user1.sUSX.deposit(maxDepositAmount, user1.address);
+        expect(await user1.sUSX.maxDeposit(user1.address)).to.eq(1);
 
         await expect(
-          // Rounded down, deposit 1 more.
+          // It will be rounded down when calculate the share amount, and current `maxDepositAmount = 1`,
+          // so deposit 2 wei asset to reach the whole mint cap.
           user1.sUSX.deposit(2, user1.address)
-        ).to.be.revertedWith("Exceeds mint cap!");
+        ).to.be.revertedWith("ERC4626: deposit more than max");
+
+        // Withdraw deposit to reset the mint cap
+        await user1.sUSX.withdraw(maxDepositAmount, user1.address, user1.address);
       });
     });
 
     describe("Withdraw", async function () {
       it("Withdraw Normally", async function() {
-        let withdrawAmount = ethers.utils.parseEther("1000");
+        // Deposit at first
+        let depositAmount = ethers.utils.parseEther("1000");
+        await user1.sUSX.deposit(depositAmount, user1.address);
+        let withdrawAmount = depositAmount.div(2);
         // Withdraw twice to check usx and sUSX
-        expect(await user1.sUSX.balanceOf(user1.address)).to.gt(withdrawAmount.mul(2));
+        expect(await user1.sUSX.maxWithdraw(user1.address)).to.gt(withdrawAmount.mul(2));
 
         // Withdraw: check USX
         await expect(
@@ -141,7 +154,7 @@ describe('sUSX', function () {
         ).to.changeTokenBalance(user1.usx, user1.address, withdrawAmount);
 
         // Withdraw: check sUSX
-        let nextRate = await user1.sUSX.getAccumulatedRateByTime((await getCurrentTime())+1);
+        let nextRate = await user1.sUSX.getRateByTime((await getCurrentTime())+1);
         // Due to math rounding up of the rate calculation, so add 1 share more.
         let shareBurnedAmount = withdrawAmount.mul(RAY).div(nextRate).add(1);
 
@@ -160,6 +173,57 @@ describe('sUSX', function () {
         await expect(
           user1.sUSX.withdraw(1, user1.address, user1.address)
         ).to.changeTokenBalance(user1.sUSX, user1.address, -1);
+      });
+
+      it("Withdraw revert when contract is paused", async function() {
+        await owner.sUSX.pause();
+        expect(await owner.sUSX.paused()).to.be.true;
+
+        await expect(
+          user1.sUSX.withdraw(1, user1.address, user1.address)
+        ).to.be.revertedWith("Pausable: paused");
+
+        // Reset pause
+        await owner.sUSX.unpause();
+        expect(await owner.sUSX.paused()).to.be.false;
+      });
+
+      it("Withdraw revert when withdraw more than max", async function() {
+        // Revert when withdraw from caller self.
+        let nextRate = await user1.sUSX.getRateByTime((await getCurrentTime())+1);
+        let user1ShareBalance = await user1.sUSX.balanceOf(user1.address);
+        let maxWithdrawAmount = user1ShareBalance.mul(nextRate).div(RAY);
+
+        await expect(
+          user1.sUSX.withdraw(maxWithdrawAmount.add(1), user1.address, user1.address)
+        ).to.be.revertedWith("ERC4626: withdraw more than max");
+
+        // Revert when withdraw from others.
+        nextRate = await user1.sUSX.getRateByTime((await getCurrentTime())+1);
+        let aliceShareBalance = await alice.sUSX.balanceOf(alice.address);
+        let maxWithdrawAmountFromAlice = aliceShareBalance.mul(nextRate).div(RAY);
+        await expect(
+          user1.sUSX.withdraw(maxWithdrawAmountFromAlice.add(1), user1.address, alice.address)
+        ).to.be.revertedWith("ERC4626: withdraw more than max");
+      });
+
+      it("Withdraw revert when caller doesn't have enough approval amount from spender", async function() {
+        // Get free token
+        let faucetAmount = ethers.utils.parseEther("1000");
+        await alice.usx.mint(alice.address, faucetAmount);
+        // Approve to sUSX to deposit
+        await alice.usx.approve(user1.sUSX.address, ethers.constants.MaxUint256);
+        // Deposit
+        await alice.sUSX.deposit(faucetAmount, alice.address);
+
+        // Revert when alice doesn't approve user1 to withdraw
+        let withdrawAmountFromAlice = faucetAmount.div(2);
+        // Ensure it will not revert by `ERC4626: withdraw more than max`
+        expect(await alice.sUSX.maxWithdraw(alice.address)).to.gt(withdrawAmountFromAlice);
+
+        await expect(
+          user1.sUSX.withdraw(withdrawAmountFromAlice, user1.address, alice.address)
+        ).to.be.revertedWith("ERC20: insufficient allowance");
       });
     });
 });
