809d08b676e8736f1c42e39cec64879175657083
Skyge <1506186404li@gmail.com>
2024-05-30 08:02:00 +0800

    :white_check_mark: Add test case for Mint and Redeem.

--- test/sUSX.test.ts
+++ test/sUSX.test.ts
@@ -111,6 +111,15 @@ describe('sUSX', function () {
         ).to.be.revertedWith("Invalid recipient address!");
       });
 
+      it("Deposit revert when caller doesn't have enough assets", async function () {
+        let depositAmount = ethers.utils.parseEther("1000");
+        expect(await alice.usx.balanceOf(alice.address)).to.lt(depositAmount);
+
+        await expect(
+          alice.sUSX.deposit(depositAmount, alice.address)
+        ).to.be.revertedWith("ERC20: burn amount exceeds balance");
+      });
+
       it("Deposit revert when exceeding mint cap", async function() {
         // Get free token
         let faucetAmount = await user1.sUSX.maxDeposit(user1.address);
@@ -136,6 +145,87 @@ describe('sUSX', function () {
 
         // Withdraw deposit to reset the mint cap
         await user1.sUSX.withdraw(maxDepositAmount, user1.address, user1.address);
+      });
+    });
+
+    describe("Mint", async function () {
+      it("Mint Normally", async function() {
+        let mintAmount = ethers.utils.parseEther("1000");
+        // Mint twice to check usx and sUSX, so user1 should have enough USX
+        expect(await user1.usx.balanceOf(user1.address)).to.gt(mintAmount.mul(2));
+        // Should not exceed mint cap
+        expect(await user1.sUSX.maxMint(user1.address)).to.gt(mintAmount.mul(2));
+
+        // Mint: check USX
+        let nextRate = await user1.sUSX.getRateByTime((await getCurrentTime())+1);
+        // Rounding down, so add 1 more asset.
+        let assetBurnedAmount = mintAmount.mul(nextRate).div(RAY).add(1);
+        await expect(
+          user1.sUSX.mint(mintAmount, user1.address)
+        ).to.changeTokenBalance(user1.usx, user1.address, assetBurnedAmount.mul(-1));
+
+        // Mint: check sUSX
+        await expect(
+          user1.sUSX.mint(mintAmount, user1.address)
+        ).to.changeTokenBalance(user1.sUSX, user1.address, mintAmount);
+      });
+
+      it("Mint 1 wei share, cost at least 2 wei asset", async function() {
+        // Mint: check USX
+        // Rounding down, so add 1 more asset.
+        await expect(
+          user1.sUSX.mint(1, user1.address)
+        ).to.changeTokenBalance(user1.usx, user1.address, -2);
+
+        // Mint: check sUSX
+        await expect(
+          user1.sUSX.mint(1, user1.address)
+        ).to.changeTokenBalance(user1.sUSX, user1.address, 1);
+      
+      });
+
+      it("Mint revert when contract is paused", async function() {
+        await owner.sUSX.pause();
+        expect(await owner.sUSX.paused()).to.be.true;
+
+        await expect(
+          user1.sUSX.mint(1, user1.address)
+        ).to.be.revertedWith("Pausable: paused");
+
+        // Reset pause
+        await owner.sUSX.unpause();
+        expect(await owner.sUSX.paused()).to.be.false;
+      });
+
+      it("Mint revert when mint more than max", async function() {
+        let maxMintAmount = await user1.sUSX.maxMint(user1.address);
+        await expect(
+          user1.sUSX.mint(maxMintAmount.add(1), user1.address)
+        ).to.be.revertedWith("ERC4626: mint more than max");
+      });
+
+      it("Mint revert when recipient is invalid", async function() {
+        // Revert due to recipient is zero address
+        await expect(
+          user1.sUSX.mint(1, ethers.constants.AddressZero)
+        ).to.be.revertedWith("Invalid recipient address!");
+
+        // Revert due to recipient is sUSX contract
+        await expect(
+          user1.sUSX.mint(1, user1.sUSX.address)
+        ).to.be.revertedWith("Invalid recipient address!");
+      });
+
+      it("Mint revert when caller doesn't have enough assets", async function () {
+        let mintAmount = ethers.utils.parseEther("1000");
+        expect(await alice.usx.balanceOf(alice.address)).to.lt(mintAmount);
+
+        // Approve to sUSX to mint
+        await alice.usx.approve(alice.sUSX.address, ethers.constants.MaxUint256);
+        await expect(
+          alice.sUSX.mint(mintAmount, alice.address)
+        ).to.be.revertedWith("ERC20: burn amount exceeds balance");
+      
       });
     });
 
@@ -207,6 +297,22 @@ describe('sUSX', function () {
         ).to.be.revertedWith("ERC4626: withdraw more than max");
       });
 
+      it("Withdraw revert when recipient is invalid", async function() {
+        // Deposit some at first
+        let depositAmount = ethers.utils.parseEther("1");
+        await user1.sUSX.deposit(depositAmount, user1.address);
+
+        // Revert due to recipient is zero address
+        await expect(
+          user1.sUSX.withdraw(1, ethers.constants.AddressZero, user1.address)
+        ).to.be.revertedWith("Invalid recipient address!");
+
+        // Revert due to recipient is sUSX contract
+        await expect(
+          user1.sUSX.withdraw(1, user1.sUSX.address, user1.address)
+        ).to.be.revertedWith("Invalid recipient address!");
+      });
+
       it("Withdraw revert when caller doesn't have enough approval amount from spender", async function() {
         // Get free token
         let faucetAmount = ethers.utils.parseEther("1000");
@@ -223,6 +329,104 @@ describe('sUSX', function () {
 
         await expect(
           user1.sUSX.withdraw(withdrawAmountFromAlice, user1.address, alice.address)
+        ).to.be.revertedWith("ERC20: insufficient allowance");
+      });
+    });
+
+    describe("Redeem", async function () {
+      it("Redeem Normally", async function() {
+        // Deposit at first
+        let depositAmount = ethers.utils.parseEther("1000");
+        await user1.sUSX.deposit(depositAmount, user1.address);
+        let redeemAmount = depositAmount.div(4);
+        // Redeem twice to check usx and sUSX
+        expect(await user1.sUSX.maxRedeem(user1.address)).to.gt(redeemAmount.mul(2));
+
+        // Redeem: check USX
+        let nextRate = await user1.sUSX.getRateByTime((await getCurrentTime())+1);
+        let assetMintedAmount = redeemAmount.mul(nextRate).div(RAY);
+        await expect(
+          user1.sUSX.redeem(redeemAmount, user1.address, user1.address)
+        ).to.changeTokenBalance(user1.usx, user1.address, assetMintedAmount);
+
+        // Redeem: check sUSX
+        await expect(
+          user1.sUSX.redeem(redeemAmount, user1.address, user1.address)
+        ).to.changeTokenBalance(user1.sUSX, user1.address, redeemAmount.mul(-1));
+      });
+
+      it("Redeem by 1 wei share, get 1 asset at least", async function() {
+        // Deposit some at first
+        let depositAmount = ethers.utils.parseEther("1");
+        await user1.sUSX.deposit(depositAmount, user1.address);
+
+        // Check USX
+        await expect(
+          user1.sUSX.redeem(1, user1.address, user1.address)
+        ).to.changeTokenBalance(user1.usx, user1.address, 1);
+
+        // Check sUSX
+        await expect(
+          user1.sUSX.redeem(1, user1.address, user1.address)
+        ).to.changeTokenBalance(user1.sUSX, user1.address, -1);
+      });
+
+      it("Redeem revert when contract is paused", async function() {
+        // Deposit some at first
+        await user1.sUSX.deposit(100, user1.address);
+
+        await owner.sUSX.pause();
+        expect(await owner.sUSX.paused()).to.be.true;
+
+        await expect(
+          user1.sUSX.redeem(1, user1.address, user1.address)
+        ).to.be.revertedWith("Pausable: paused");
+
+        // Reset pause
+        await owner.sUSX.unpause();
+        expect(await owner.sUSX.paused()).to.be.false;
+      });
+
+      it("Redeem revert when redeem more than max", async function() {
+        let maxRedeemAmount = await user1.sUSX.maxRedeem(user1.address);
+
+        await expect(
+          user1.sUSX.redeem(maxRedeemAmount.add(1), user1.address, user1.address)
+        ).to.be.revertedWith("ERC4626: redeem more than max");
+      });
+
+      it("Redeem revert when recipient is invalid", async function() {
+        // Deposit some at first
+        let depositAmount = ethers.utils.parseEther("1");
+        await user1.sUSX.deposit(depositAmount, user1.address);
+
+        // Revert due to recipient is zero address
+        await expect(
+          user1.sUSX.redeem(1, ethers.constants.AddressZero, user1.address)
+        ).to.be.revertedWith("Invalid recipient address!");
+
+        // Revert due to recipient is sUSX contract
+        await expect(
+          user1.sUSX.redeem(1, user1.sUSX.address, user1.address)
+        ).to.be.revertedWith("Invalid recipient address!");
+      });
+
+      it("Redeem revert when caller doesn't have enough approval amount from spender", async function() {
+        // Get free token
+        let faucetAmount = ethers.utils.parseEther("1000");
+        await alice.usx.mint(alice.address, faucetAmount);
+        // Approve to sUSX to deposit
+        await alice.usx.approve(user1.sUSX.address, ethers.constants.MaxUint256);
+        // Deposit
+        await alice.sUSX.deposit(faucetAmount, alice.address);
+
+        // Revert when alice doesn't approve user1 to redeem
+        let redeemAmountFromAlice = faucetAmount.div(2);
+        // Ensure it will not revert by `ERC4626: redeem more than max`
+        expect(await alice.sUSX.maxRedeem(alice.address)).to.gt(redeemAmountFromAlice);
+
+        await expect(
+          user1.sUSX.withdraw(redeemAmountFromAlice, user1.address, alice.address)
         ).to.be.revertedWith("ERC20: insufficient allowance");
       });
     });
