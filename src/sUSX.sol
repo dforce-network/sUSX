d15741426092d30cc8f1ce0a8fa891eb968483f5
Skyge <1506186404li@gmail.com>
2024-06-12 09:34:58 +0800

    :lock: Fixed compiler version.

--- src/sUSX.sol
+++ src/sUSX.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.10;
+pragma solidity 0.8.10;
 
 import {AccessControlEnumerableUpgradeable} from "@openzeppelin/contracts-upgradeable/access/AccessControlEnumerableUpgradeable.sol";
 import {ERC20PermitUpgradeable} from "@openzeppelin/contracts-upgradeable/token/ERC20/extensions/ERC20PermitUpgradeable.sol";
